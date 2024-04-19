c66a83b17f6f392b46d50a3f68937775d511a293
Prasna Aryal <prasna101@gmail.com>
2024-04-19 13:13:33 +0545

    card component,home added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,17 +1,96 @@
-import { useState } from "react";
-import Banner from "../components/Banner"
+import { useEffect, useState } from "react";
+import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Jobs from "./Jobs";
 
 const Home = () => {
-  const [query,setQuery]=useState("");
-    const handleInputChange=(event)=>{
-        setQuery(event.target.value)
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
 
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, []);
+
+  const [query, setQuery] = useState("");
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  //filter jobs by title
+  const filteredItems = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+  // console.log(filteredItems)
+
+  //Radiobuttons
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //buttin based filtering
+  const handleClikc = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  //main function
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
+
+    //filtering input items
+    if (query) {
+      filteredJobs = filteredItems;
     }
-    console.log(query)
+
+    if (selected) {
+      filteredJobs = filteredJobs.filter(
+        ({
+          jobLocation,
+          maxPrice,
+          experienceLevel,
+          salaryType,
+          employmentType,
+          postingDate,
+        }) =>
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+          parseInt(maxPrice) <= parseInt(selected) ||
+          salaryType.toLowerCase() === selected.toLowerCase() ||
+          employmentType.toLowerCase() === selected.toLowerCase()
+        // experienceLevel === selected ||
+        // jobLocation === selected
+      );
+      console.log(filteredData);
+    }
+
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />);
+  };
+
+  const result = filteredData(jobs, selectedCategory, query);
+
   return (
-    <Banner query={query} handleInputChange={handleInputChange}/>
+    <div>
+      <Banner query={query} handleInputChange={handleInputChange} />
+      <div>
+        <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
 
-  )
-}
+          {/* left side */}
+          <div className="bg-white p-4 rounded">Left</div>
 
-export default Home
+          {/* job cards */}
+          <div className="col-span-2 bg-white p-4 rounded-sm">
+            <Jobs result={result} />
+          </div>
+
+          {/* right side */}
+
+          <div className="bg-white p-4 rounded">Right</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router/Router";
 import './index.css'
+import './App.css'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
