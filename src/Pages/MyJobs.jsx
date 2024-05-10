abed09f6ad41d13f41060d77c8064621d02185b0
Prasna Aryal <prasna101@gmail.com>
2024-05-10 17:13:09 +0545

    myjobs page created

---
+++ src/Pages/MyJobs.jsx
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const MyJobs = () => {
+  const [jobs, setJobs] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleSearch = () => {
+    const filter = jobs.filter(
+      (job) =>
+        job.jobTitle.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+    );
+    console.log(filter);
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="max-w-screen-2xl container mx-autp xl:px-24 px-4">
+      <div className="my-jobs-container">
+        <h1 className="text-center p-4">All My Jobs</h1>
+        <div className="search-box p-2 text-center mb-2">
+          <input
+            onChange={(e) => setSearchText(e.target.value)}
+            type="text"
+            name="search"
+            id="search"
+            className="py-2 pl-3 border focus:outline-none lg:w-6/12 mb-4 w-full"
+          />
+          <button
+            className="bg-blue-400 text-white font-semibold px-8 py-2 rounded-sm mb-4"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      <section className="py-1 bg-blueGray-50">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 mx-auto mt-24">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 className="font-semibold text-base text-blueGray-700">
+                    All Jobs
+                  </h3>
+                </div>
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                  <button
+                    className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    See all
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="block w-full overflow-x-auto">
+              <table className="items-center bg-transparent w-full border-collapse ">
+                <thead>
+                  <tr>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      NO
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Title
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Company Name
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Salary
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      EDIT
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      DELETE
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {/* {jobs.map((job, index) => ( */}
+                    <tr 
+                    // key={index}
+                    >
+                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                        {/* {index + 1} */}
+                        123
+                      </th>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
+                        {/* {job.jobTitle} */}
+                        Developer
+                      </td>
+                      <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {/* {job.companyName} */}
+                        Softwarica
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {/* ${job.minPrice}-${job.maxPrice} */}
+                        8000
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <button>
+                          <Link to={""}>Edit</Link>
+                        </button>
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <button className="bg-red-700 py-2 px-6 text-white rounded-sm ">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  {/* ))} */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MyJobs;
