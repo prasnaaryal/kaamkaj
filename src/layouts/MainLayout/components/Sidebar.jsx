b1226860d958280861b4d3da7f08c47864901005
Prasna Aryal <prasna101@gmail.com>
2024-06-07 08:45:40 +0545

    sidebar edited

--- src/layouts/MainLayout/components/Sidebar.jsx
+++ src/layouts/MainLayout/components/Sidebar.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Menu, MenuItem } from 'react-pro-sidebar';
-import { FaTachometerAlt, FaBriefcase, FaBookmark, FaUser, FaCog } from 'react-icons/fa'; // Importing icons
-import { Link } from 'react-router-dom'; // Importing Link from react-router-dom
-import { Outlet } from 'react-router-dom'; // Importing Outlet from react-router-dom
-import { MdDashboard } from 'react-icons/md';
+import React from "react";
+import { Menu, MenuItem } from "react-pro-sidebar";
+import {
+  FaTachometerAlt,
+  FaBriefcase,
+  FaBookmark,
+  FaUser,
+  FaCog,
+} from "react-icons/fa";
+import { Link, Outlet } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
 
 const Sidebar = () => {
   const menuItems = [
-    { href: "/manage/dashboard", icon: MdDashboard 
-    , label: "Dashboard" },
+    { href: "/manage/dashboard", icon: MdDashboard, label: "Dashboard" },
     { href: "/manage/my-job", icon: FaBriefcase, label: "Jobs" },
     { href: "/manage/saved-jobs", icon: FaBookmark, label: "Saved Jobs" },
     { href: "/manage/edit-profile", icon: FaUser, label: "Profile" },
-    { href: "/manage/settings", icon: FaCog, label: "Settings" }
+    { href: "/manage/settings", icon: FaCog, label: "Settings" },
   ];
 
   return (
@@ -21,10 +25,15 @@ const Sidebar = () => {
         <div className="p-4">
           <Menu iconShape="square">
             {menuItems.map((item, index) => (
-              <MenuItem key={index} className="hover:bg-blue-100 text-gray-800 flex items-center p-2 my-2 rounded-md">
+              <MenuItem
+                key={index}
+                className={`hover:bg-blue-100 text-gray-800 flex items-center p-3 my-2 rounded-md transition-colors duration-200 ease-in-out ${
+                  window.location.pathname === item.href ? "bg-blue-200" : ""
+                }`}
+              >
                 <Link to={item.href} className="flex items-center">
-                  {item.icon && <item.icon className="w-5 h-5" />} {/* Ensure the icon is rendered */}
-                  <span className="ml-3">{item.label}</span>
+                  {item.icon && <item.icon className="w-6 h-6" />}
+                  <span className="ml-3 text-lg">{item.label}</span>
                 </Link>
               </MenuItem>
             ))}
