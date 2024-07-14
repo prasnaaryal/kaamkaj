fe27e96387f43b4de43b7383302c7338a9ffb9c2
prasnaaryal <prasna101@gmail.com>
2024-07-14 13:20:35 +0545

    search in salary estimate page

--- src/Pages/SalaryPage.jsx
+++ src/Pages/SalaryPage.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
-import PageHeader from "../components/PageHeader";
 import axiosInstance from "../config/axiosConfig";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const SalaryPage = () => {
   const [searchText, setSearchText] = useState("");
   const [jobs, setJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,21 +13,50 @@ const SalaryPage = () => {
       .get("/job/getalljobs")
       .then((res) => {
         setJobs(res.data);
+        setFilteredJobs(res.data);
       })
       .catch((error) => {
         console.error("Error fetching jobs:", error);
       });
   }, []);
 
+  useEffect(() => {
+    handleSearch();
+  }, [searchText]);
+
   const handleSearch = () => {
-    const filter = jobs.filter((job) =>
-      job.category.toLowerCase().includes(searchText.toLowerCase())
+    const searchTerm = searchText.toLowerCase();
+    const matchedJobs = jobs.filter(
+      (job) => job.category && job.category.toLowerCase().includes(searchTerm)
     );
-    setJobs(filter);
+
+    const otherJobs = jobs.filter(
+      (job) => job.category && !job.category.toLowerCase().includes(searchTerm)
+    );
+
+    setFilteredJobs([...matchedJobs, ...otherJobs]);
   };
 
-  const jobCountsByCategory = jobs.reduce((acc, job) => {
-    acc[job.category] = (acc[job.category] || 0) + 1;
+  const jobCountsByCategory = filteredJobs.reduce((acc, job) => {
+    if (job.category) {
+      acc[job.category] = (acc[job.category] || 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  const averageSalaryByCategory = filteredJobs.reduce((acc, job) => {
+    if (job.category) {
+      const minSalary = parseInt(job.minSalary);
+      const maxSalary = parseInt(job.maxSalary);
+      const avgSalary = (minSalary + maxSalary) / 2;
+
+      if (!acc[job.category]) {
+        acc[job.category] = { totalSalary: avgSalary, count: 1 };
+      } else {
+        acc[job.category].totalSalary += avgSalary;
+        acc[job.category].count += 1;
+      }
+    }
     return acc;
   }, {});
 
@@ -37,7 +66,11 @@ const SalaryPage = () => {
 
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
-      <PageHeader title={"Estimate Salary"} path={"Salary"} />
+      <div className="flex w-full justify-center py-10">
+        <h1 className="text-4xl text-blue-500 font-semibold">
+          Estimate Salary
+        </h1>
+      </div>
 
       <div className="mt-5">
         <div className="search-box p-2 text-center mb-2">
@@ -46,11 +79,12 @@ const SalaryPage = () => {
             name="search"
             id="search"
             className="py-2 pl-3 border focus:outline-none lg:w-6/12 mb-4 w-full"
+            value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
           />
           <button
             onClick={handleSearch}
-            className="bg-blue-200 text-white font-semibold px-8 py-2 rounded-sm mb-4"
+            className="bg-blue-500 text-white font-semibold px-8 py-2 rounded-sm mb-4"
           >
             Search
           </button>
@@ -60,10 +94,17 @@ const SalaryPage = () => {
       {/* salary display card */}
       <div className="grid llg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-12 my-12 items-center">
         {Object.keys(jobCountsByCategory).map((category, index) => (
-          <div key={index} className="shadow border border-black-500 px-4 py-8">
+          <div key={index} className="shadow border rounded-lg px-4 py-8">
             <h4 className="font-semibold text-xl">{category}</h4>
-            <p className="my-2 font-medium text-black-300 text-lg">
+            <p className="my-2 font-medium text-black/40 text-sm">
               {jobCountsByCategory[category]} Jobs
+            </p>
+            <p className="my-2 font-medium text-black/60 text-base">
+              Average Salary:{" "}
+              {(
+                averageSalaryByCategory[category].totalSalary /
+                averageSalaryByCategory[category].count
+              ).toFixed(2)}
             </p>
             <div className="flex flex-wrap gap-4">
               <button
