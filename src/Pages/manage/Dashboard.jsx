c1f304e1cbcb56da9dcf6e024cfc0e2028c5d88e
prasnaaryal <prasna101@gmail.com>
2024-07-14 20:56:37 +0545

    dashboard changes

--- src/Pages/manage/Dashboard.jsx
+++ src/Pages/manage/Dashboard.jsx
@@ -1,44 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../config/axiosConfig";
+import { IoIosPeople } from "react-icons/io";
 
 const Dashboard = () => {
+  const [postedJobsCount, setPostedJobsCount] = useState(0);
+  const [totalApplicantsCount, setTotalApplicantsCount] = useState(0);
   const [appliedJobsCount, setAppliedJobsCount] = useState(0);
   const [savedJobsCount, setSavedJobsCount] = useState(0);
   const [isProfileIncomplete, setIsProfileIncomplete] = useState(false);
+  const [userRole, setUserRole] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchJobCounts = async () => {
-      try {
-        const token = localStorage.getItem("accessToken");
-
-        const appliedJobsResponse = await axiosInstance.post(
-          "/job/get-applied-job-by-user",
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setAppliedJobsCount(appliedJobsResponse.data.length);
-
-        const savedJobsResponse = await axiosInstance.get(
-          "/job/favorite/getall",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setSavedJobsCount(savedJobsResponse.data.length);
-      } catch (error) {
-        console.error("Error fetching job counts:", error);
-      }
-    };
-
-    const checkProfileCompletion = async () => {
+    const fetchUserData = async () => {
       try {
         const token = localStorage.getItem("accessToken");
         const userResponse = await axiosInstance.get("/user/load-user", {
@@ -47,16 +22,63 @@ const Dashboard = () => {
           },
         });
 
-        if (!userResponse.data.user.cv) {
-          setIsProfileIncomplete(true);
+        const user = userResponse.data.user;
+        setUserRole(user.role);
+
+        if (user.role === "company") {
+          const companyName = user.fullName;
+          const jobsResponse = await axiosInstance.get(
+            `/job/company/${encodeURIComponent(companyName)}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          const jobs = jobsResponse.data;
+          setPostedJobsCount(jobs.length);
+          const applicantsCount = jobs.reduce(
+            (acc, job) => acc + job.applicants.length,
+            0
+          );
+          setTotalApplicantsCount(applicantsCount);
+
+          if (!user.aboutUs) {
+            setIsProfileIncomplete(true);
+          }
+        } else if (user.role === "applicant") {
+          const appliedJobsResponse = await axiosInstance.post(
+            "/job/get-applied-job-by-user",
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setAppliedJobsCount(appliedJobsResponse.data.length);
+
+          const savedJobsResponse = await axiosInstance.get(
+            "/job/favorite/getall",
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setSavedJobsCount(savedJobsResponse.data.length);
+
+          if (!user.cv) {
+            setIsProfileIncomplete(true);
+          }
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
 
-    fetchJobCounts();
-    checkProfileCompletion();
+    fetchUserData();
   }, []);
 
   const handleEditProfile = () => {
@@ -70,35 +92,82 @@ const Dashboard = () => {
           <h1 className="font-semibold text-2xl">Welcome Back!!</h1>
           <div className="border-[1px] border-blue-500"></div>
         </div>
-        <div className="flex justify-between gap-3 w-full pb-6">
-          <div className="w-80 shadow-md rounded-lg p-4 flex items-center bg-[#E7F0FA]">
-            <div className="grid grid-cols-12 gap-2 w-full px-6">
-              <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
-                <h2 className="font-semibold text-2xl">{appliedJobsCount}</h2>
-                <p className="font-semibold text-xl text-gray-400">
-                  Applied jobs
-                </p>
-              </div>
-              <div className="col-span-3 flex items-center justify-center">
-                <img className="w-16 h-14" src="/images/Icon (4).png" alt="" />
+        {userRole === "company" && (
+          <div className="flex justify-between gap-3 w-full pb-6">
+            <div className="w-80 shadow-md rounded-lg p-4 flex items-center bg-[#E7F0FA]">
+              <div className="grid grid-cols-12 gap-2 w-full px-6">
+                <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
+                  <h2 className="font-semibold text-2xl">{postedJobsCount}</h2>
+                  <p className="font-semibold text-xl text-gray-400">
+                    Posted Jobs
+                  </p>
+                </div>
+                <div className="col-span-3 flex items-center justify-center">
+                  <img
+                    className="w-16 h-14"
+                    src="/images/Icon (4).png"
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className=" w-80 shadow-md rounded-lg p-4 flex items-center bg-[#FFF6E6]">
-            <div className="grid grid-cols-12 gap-2 w-full px-6">
-              <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
-                <h2 className="font-semibold text-2xl">{savedJobsCount}</h2>
-                <p className="font-semibold text-xl text-gray-400">
-                  Saved Jobs
-                </p>
-              </div>
-              <div className="col-span-3 flex items-center justify-center">
-                <img className="w-16 h-14" src="/images/Icon (5).png" alt="" />
+            <div className="w-80 shadow-md rounded-lg p-4 flex items-center bg-[#FFF6E6]">
+              <div className="grid grid-cols-12 gap-2 w-full px-6">
+                <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
+                  <h2 className="font-semibold text-2xl">
+                    {totalApplicantsCount}
+                  </h2>
+                  <p className="font-semibold text-xl text-gray-400">
+                    Total Applicants
+                  </p>
+                </div>
+                <div className="col-span-3 h-16 rounded-md flex bg-white items-center justify-center">
+                  <IoIosPeople className="w-8 h-20" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
+        {userRole === "applicant" && (
+          <div className="flex justify-between gap-3 w-full pb-6">
+            <div className="w-80 shadow-md rounded-lg p-4 flex items-center bg-[#E7F0FA]">
+              <div className="grid grid-cols-12 gap-2 w-full px-6">
+                <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
+                  <h2 className="font-semibold text-2xl">{appliedJobsCount}</h2>
+                  <p className="font-semibold text-xl text-gray-400">
+                    Applied Jobs
+                  </p>
+                </div>
+                <div className="col-span-3 flex items-center justify-center">
+                  <img
+                    className="w-16 h-14"
+                    src="/images/Icon (4).png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="w-80 shadow-md rounded-lg p-4 flex items-center bg-[#FFF6E6]">
+              <div className="grid grid-cols-12 gap-2 w-full px-6">
+                <div className="col-span-9 flex flex-col gap-2 items-center justify-center">
+                  <h2 className="font-semibold text-2xl">{savedJobsCount}</h2>
+                  <p className="font-semibold text-xl text-gray-400">
+                    Saved Jobs
+                  </p>
+                </div>
+                <div className="col-span-3 flex items-center justify-center">
+                  <img
+                    className="w-16 h-14"
+                    src="/images/Icon (5).png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         {isProfileIncomplete && (
           <div className="shadow-md rounded-lg p-4 flex items-center bg-[#E05151] justify-between">
             <div className="flex items-center gap-6">
